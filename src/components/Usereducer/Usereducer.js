2e3082f8059302e7a8f932319525bbf485e8fa66
sajidur <sajidur.inbox@gmail.com>
2021-07-02 09:01:33 +0600

    hooks done

--- src/components/Usereducer/Usereducer.js
+++ src/components/Usereducer/Usereducer.js
@@ -1,12 +1,28 @@
-
+import {useReducer} from "react"
 import {Link} from "react-router-dom"
 
+const initialState = {count : 0}
+
+const reducer = (state, action)=>{
+    switch(action.type){
+        case 'increment':
+            return {count: state.count + 1}
+        case 'decrement':
+            return {count: state.count - 1}
+        default:
+            return state
+    }
+}
+
 const Usereducer = ()=>{
+    const [state, dispatch] = useReducer(reducer, initialState)
     return (
         <div>
             <Link className="HomeBtn" to="/">Home</Link>
 
-
+            <button onClick={()=> dispatch({type: 'increment'})}>+</button>
+            <h3>{state.count}</h3>
+            <button onClick={()=> dispatch({type: 'decrement'})}>-</button>
         </div>
     )
 }
